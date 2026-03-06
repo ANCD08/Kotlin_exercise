f86f3f4120f7b488e4cff855f558fe506a3b3226
ANCD08 <abissicyrielle9@gmail.com>
2026-03-06 14:30:42 +0100

    Delete CollectionExercise directory

--- CollectionExercise/src/main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val words = listOf("apple", "cat", "banana", "dog", "elephant")
-
-    val wordLengths = words.associateWith { it.length }
-
-    wordLengths
-        .filter { (_, length) -> length > 4 }
-        .forEach { (word, length) -> println("$word has length $length") }
-}
